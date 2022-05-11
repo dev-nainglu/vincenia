3c99b991d86d823d50907360e3aafeb2c7a34e12
Nay Linn Oo <naylinnoo.1@gmail.com>
2022-05-11 15:00:15 +0630

    removed marquee

--- components/Home/ComingSoon.tsx
+++ components/Home/ComingSoon.tsx
@@ -1,13 +1,8 @@
 import { NextPage } from 'next';
-import VNCA from '../../public/assets/images/VCNA.jpg';
-import Marquee from 'react-fast-marquee';
 
 const ComingSoon: NextPage = () => {
 	return (
 		<div>
-			<Marquee gradient={false} className="h-12">
-				<img src={VNCA.src} alt="" />
-			</Marquee>
 			<div className="container py-6">
 				<div className="grid lg:grid-cols-4 grid-cols-2 gap-4">
 					<div style={{ backgroundColor: '#3F3F3F' }} className="rounded-lg my-6 px-6 py-4">
@@ -28,9 +23,6 @@ const ComingSoon: NextPage = () => {
 					</div>
 				</div>
 			</div>
-			<Marquee gradient={false} direction="right" className="h-12">
-				<img src={VNCA.src} alt="" />
-			</Marquee>
 		</div>
 	);
 };
